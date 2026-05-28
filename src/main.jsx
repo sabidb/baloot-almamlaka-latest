54a000b2f6971e07a604c795c9b91e993e916ac8
sabidb <8742sabithsaleem@gmail.com>
2026-05-28 07:20:17 +0300

    test1.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div style={{color:'white',padding:20,fontSize:24}}>Test</div>
+    <App />
   </React.StrictMode>,
 )
