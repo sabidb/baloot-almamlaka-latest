3c342ebbe759aa9edb0b6ee6e61964daf60b0ff0
sabidb <8742sabithsaleem@gmail.com>
2026-05-28 07:18:52 +0300

    testvsave

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import AdminPanel from './AdminPanel.jsx'
 import './index.css'
-
-const isAdmin = window.location.pathname === '/admin';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isAdmin ? <AdminPanel /> : <App />}
+    <div style={{color:'white',padding:20,fontSize:24}}>Test</div>
   </React.StrictMode>,
 )
